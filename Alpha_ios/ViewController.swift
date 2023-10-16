3085959d6fd350dd500359d076770d2ecd9e8d7a
Vladislav <vladislav_gabaraev@mail.ru>
2023-10-16 19:42:31 +0300

    added url session 0

--- Alpha_ios/ViewController.swift
+++ Alpha_ios/ViewController.swift
@@ -9,10 +9,24 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemPink
+        
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard let data = data,
+                  let response,
+                    error == nil
+            else {
+                return
+            }
+            let str = String(data: data, encoding:  .utf8)
+            print(" Your data is: \(str ?? "")");
+        }).resume()
+        //completionHandler выполняет какое-то действие в тот момент, когда мы получили данные с сервера.
     }
 
 
